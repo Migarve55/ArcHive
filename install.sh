ccaf9e8fba67e3457aac479d8d0e808724f5eec4
Miguel Garnacho Velez <migarve55@gmail.com>
2020-01-29 22:13:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat packages.txt | xargs pacman -S
 
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3blocks
-mkdir -p ~/Images/Wallpapers
+mkdir -p ~/images/wallpapers
 mkdir -p ~/scripts
 
 # Copy files
@@ -17,6 +17,6 @@ cp i3 ~/.config/i3/config
 cp i3status-rs-config ~/.config/i3/status.toml
 cp Xresources ~/.Xresources
 cp vimrc ~/.vimrc
-cp Wallpapers/* ~/Images/Wallpapers/
+cp Wallpapers/* ~/images/wallpapers/
 cp i3blocks ~/.config/i3blocks/config
 cp scripts/* ~/scripts/ 
