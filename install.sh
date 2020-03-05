fde6dabd56f89c6ffe4e95793083f90ca60b10cd
Miguel Garnacho Velez <migarve55@gmail.com>
2020-03-05 18:28:50 +0100

    Better zsh

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ cat packages.txt | xargs pacman -S
 echo "Changing the shell"
 chsh -s /bin/zsh
 
+echo "Setting oh-my-zsh"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Config files
 
 echo "Configuring dotfiles..."
